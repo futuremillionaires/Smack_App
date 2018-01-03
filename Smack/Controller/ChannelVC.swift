ec1b12f2ea3990e4a38bf52eb7bb7a614a4c51e6
RD <rajudhumne@gmail.com>
2018-01-03 13:13:52 +0530

    LoggedIn Interface

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ChannelVC: UIViewController {
-    @IBOutlet weak var loginBtn: UILabel!
+   
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
       self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 85
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_:)), name:NOTIF_USER_DATA_DID_CHANGE , object: nil)
     }
-    @IBAction func loginBtnpressed(_ sender: Any) {
+  
+    @IBAction func loginBtnPressed(_ sender: Any) {
         
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
-    
+    @objc func userDataDidChanged(_ notif:Notification){
+        if AuthServices.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named:UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(componenets: UserDataService.instance.avatarColor)
+        }else {
+            
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named:"menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+
+
+        
+    }
 }
+
+
+
+
+
+
+
--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -39,6 +39,7 @@ class CreateAccountVC: UIViewController {
         let g = CGFloat(arc4random_uniform(255)) / 255
         let b = CGFloat(arc4random_uniform(255)) / 255
         bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        avatarColor = "[\(r),\(b),\(g),1]"
         UIView.animate(withDuration: 0.2) {
         self.userImage.backgroundColor = self.bgColor
         }
